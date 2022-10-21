b9ffca73c67374b53813cffa1208b422f23d05da
tyler ferre <ferre.tyler1@gmail.com>
2022-10-20 19:09:41 -0600

    finished other pages

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ form.addEventListener("submit", (e) => {
 
     form.search.value = ""
 })
+
+//color picker for fun
+
+// var enter = document.getElementById("enter")
+// .addEventListener("click", function(){
+//     var colorPicked = document.getElementById("colorPic").value;
+//     var bodyInput = document.body
+//     var changedColor = bodyInput.style.backgroundColor = colorPicked
+// }
+// )
